56745138afdaf21dde14a127682c2b8d2a1fba3f
Lynne-Craige <lynnecraigesaranghae@gmail.com>
2025-05-25 09:47:49 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,19 +6,25 @@ use App\Models\Appointment;
 use App\Observers\AppointmentObserver;
 use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Barryvdh\Debugbar\ServiceProvider as DebugbarServiceProvider;
 
-class AppServiceProvider 
+class AppServiceProvider extends ServiceProvider
 {
-
-
-
-
-    public function boot(): void
+    
+    public function register()
 {
-   // if (config('app.env') === 'production') {
-   //     URL::forceScheme('https');
-  //  }
+    if ($this->app->environment('local')) {
+        $this->app->register(DebugbarServiceProvider::class);
+    }
 }
+
+    
+    public function boot()
+    {
+        Appointment::observe(AppointmentObserver::class);
+        
+    }
+
+
 }
