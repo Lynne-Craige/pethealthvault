8ae052d7fb8293549d3f748ab8497317ab857f8e
Lynne-Craige <lynnecraigesaranghae@gmail.com>
2025-05-25 18:45:17 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Models\Contracts\HasName;
+use Filament\Panel; 
+
 
 class User extends Authenticatable implements FilamentUser, HasName, MustVerifyEmail, CanResetPassword
 {
