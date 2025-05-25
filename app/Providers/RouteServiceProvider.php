868101059f897c714eb7786e3313ade9255038cc
Lynne-Craige <lynnecraigesaranghae@gmail.com>
2025-05-25 13:43:36 +0800

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -23,9 +23,6 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
 
-            Route::prefix('api')
-                ->middleware('api')
-                ->group(base_path('routes/api.php'));
         });
     }
 }
