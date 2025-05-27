254e14c101a7f04311c7825f326deb92d223ee9d
Lynne-Craige <lynnecraigesaranghae@gmail.com>
2025-05-27 16:54:59 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,14 +9,14 @@ use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use App\Filament\Responses\Auth\CustomLogoutResponse;
-use Filament\Http\Responses\Auth\Contracts\LogoutResponse;
+use Filament\Http\Responses\Auth\Contracts\LogoutResponse as LogoutResponseContract;
 
 class AppServiceProvider extends ServiceProvider
 {
     
-   public function register()
+  public function register()
 {
-    $this->app->singleton(LogoutResponse::class, CustomLogoutResponse::class);
+    $this->app->singleton(LogoutResponseContract::class, CustomLogoutResponse::class);
 }
     
 
