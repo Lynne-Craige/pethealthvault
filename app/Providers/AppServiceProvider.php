a4e68772469ffaa7a8bcde32eabdd04230ea9219
Lynne-Craige <lynnecraigesaranghae@gmail.com>
2025-05-27 15:58:29 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,16 +8,17 @@ use App\Observers\AppointmentObserver;
 use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Fortify\Contracts\LogoutResponse;
-use App\Http\Responses\LogoutResponse as CustomLogoutResponse;
+use App\Filament\Responses\Auth\CustomLogoutResponse;
+use Filament\Http\Responses\Auth\Contracts\LogoutResponse;
 
 class AppServiceProvider extends ServiceProvider
 {
     
-    public function register()
+   public function register()
 {
     $this->app->singleton(LogoutResponse::class, CustomLogoutResponse::class);
 }
+    
 
     
     public function boot()
